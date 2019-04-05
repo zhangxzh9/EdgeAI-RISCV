42489f5c8e9450efc101cf47898e590d1c965358
zhangxianzhi <zxzncdx28@163.com>
2019-04-05 17:20:27 +0800

    更改项目结构

---
+++ recognition-c-win-vs2019/recognition_float.c
@@ -0,0 +1,103 @@
+// See LICENSE for license details.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "data.h"
+
+float my_exp(float x)
+{
+	x = 1.0 + x / 256.0;
+	for (int i = 0; i < 8; i++) {
+		x *= x;
+	}
+	return x;
+}
+
+//对每一行进行softmax
+void softmax(float* x, uint8_t row, uint8_t column)
+{
+	uint8_t k, j;
+	for (j = 0; j < row; ++j)
+	{
+		float max = 0.0;
+		float sum = 0.0;
+		for (k = 0; k < column; ++k)
+			if (max < x[k + j * column])
+				max = x[k + j * column];
+		for (k = 0; k < column; ++k)
+		{
+			x[k + j * column] = my_exp(x[k + j * column] - max);    // prevent data overflow
+			sum += x[k + j * column];
+		}
+		for (k = 0; k < column; ++k) 
+			x[k + j * column] /= sum;
+	}
+}   //row*column
+uint8_t max_probability(float* x) {
+	uint8_t num_max = 0, i = 0;
+	float num = 0.0;
+	for (i = 0; i < Out; i++) {
+		if (num <= *x) {
+			num = *x;
+			num_max = i;
+		}
+		x++;
+	}
+	return num_max;
+}
+float Output[Out] = { 0.0 };
+uint8_t  result(float * var1)
+{
+	uint8_t i;
+	uint16_t j;
+	float sum=0.0;
+	float data[In] = {0.0} , o[Neuron] = { 0.0 };
+	for (j = 0; j < In; j++) {
+		data[j] = *var1;
+		var1++;
+	}
+	/*preprocessing the data*/
+	for (i = 0; i < Neuron; ++i) {
+		sum = 0;
+		for (j=0;j< In;j++) {
+			sum += In_Neuron[j][i] * data[j];
+		}
+		o[i] = sum + bias0[i];
+		o[i] = (o[i] >= 0) ? o[i] : 0;//relu activaction
+	}
+	sum = 0;
+	for (i = 0; i < Out; ++i) {
+		sum = 0;
+		for (j = 0; j < Neuron; j++) {
+			sum += Neuron_Out[j][i] * o[j];
+		}	
+		Output[i] = sum + bias1[i];
+	}
+	softmax(Output, 1, Out);
+	return max_probability(Output);
+}
+
+int main(int argc, char **argv)
+{
+	uint16_t i = 0;
+	float var[In] = {0.0};
+	uint8_t res,j;
+
+	while (1) {
+		for (j = 0; j < 3; j++) {
+			for (i = 0; i < 784; i++)
+			{
+				var[i] = test_data[j][i] / 255.0;
+			}
+			res = result(var);
+			for (i = 0; i < Out; i++)
+			{
+				printf("%d:%lf\n", i, Output[i]);
+			}
+			printf("prection is %d\n\n", res);
+		}
+	}
+	
+	return 0;
+}
--- recognition_quant-c-win-vs2019/data.h
+++ recognition_quant-c-win-vs2019/data.h
@@ -1,9 +1,10 @@
 #pragma once
 #ifndef __DATA__H
 #define __DATA__H
-//#define RISCV_E203
 
-#ifndef RISCV_E203
+#define RISCV_VS
+
+#ifdef RISCV_VS
 
 typedef unsigned short int uint16_t;
 typedef unsigned int       uint32_t;
@@ -14,17 +15,19 @@ typedef int       int32_t;
 typedef long int  int64_t;
 #endif
 
-typedef unsigned char uint8_t;
+typedef unsigned char      uint8_t;
 typedef short int int16_t;
 
 #define In 784
 #define Out 10
 #define Neuron 48
 
+extern uint8_t test_data[3][784];
+extern uint8_t In_Neuron[In][Neuron];
+extern uint8_t Neuron_Out[Neuron][Out];
+extern int16_t bias0[Neuron];
+extern float bias1[Out];
+extern uint8_t b[2];
+extern float k[2];
 
-extern const uint8_t test_data[3][784];
-extern const float In_Neuron[In][Neuron];
-extern const float Neuron_Out[Neuron][Out];
-extern const float bias0[Neuron];
-extern const float bias1[Out];
 #endif // !__DATA__H
