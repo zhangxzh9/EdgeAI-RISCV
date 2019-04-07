0cafee66dee2ad64f416198ce15198863f287f76
弦智 张 <838247653@qq.com>
2019-04-08 00:03:53 +0800

    完成URAT接口设计

--- recognition_quant-c-RISCV-terminal/urat_sentdata.py
+++ recognition_quant-c-RISCV-terminal/urat_sentdata.py
@@ -1,51 +1,74 @@
 #usr/bin/python3
 # -*- coding: utf-8 -*-
-import serial
+from serial import Serial
 from time import sleep
  
-ser = serial.Serial('/dev/ttyUSB2', 115200, timeout=0.5)
+ser = Serial('/dev/ttyUSB1', 115200, timeout=0.5)
  
  
-def recv(serial):
-        data = ''
-        i = 0
-        while True:
-            data = serial.read(1000)
-            if data == b'':
-                i = i + 1
-                if(i<20):
-                    pass
-                else:
-                    print('time out!')
-                    break
+def recv_all(serial):
+    data = ''
+    i = 0
+    while True:
+        data = serial.read_all()
+        if data == b'':
+            i = i + 1
+            if(i<10):
+                pass
             else:
+                print('time out!')
                 break
-            sleep(0.5)
-        return data
+        else:
+            break
+        sleep(0.5)
+    return data
  
+def recv_line(serial):
+    data = ''
+    i = 0
+    while True:
+        data = serial.readline()
+        if data == b'':
+            i = i + 1
+            if(i<10):
+                pass
+            else:
+                print('time out!')
+                break
+        else:
+            break
+        sleep(0.1)
+    return data
 while True:
-        data = recv(ser)
-        print(data.decode("utf-8"))
-        while True:        
-            ser.write(b'b')
-            sleep(0.5)
-            data = ser.readline()
-            print(data)
-            if data == b'\rok1\r\n':
-                ser.write(b'e')
-                sleep(0.5)
-                data = ser.readline()
-                print(data)
-                if data == b'\rok2\r\n':
-                    break
-                else:
-                    print('fail to handshake')
+    data = recv_line(ser)
+    print(bytes.decode(data))
+    if data ==b'\rfinish\r\n':
+        break
+while True:
+    while True:
+        ser.write(b'b')
+        data = recv_line(ser)
+        #print('read data is: ' + str(data))
+        if data == b'ok1\r\n' or data == b'\rok1\r\n':
+            ser.write(b'e')
+            data = recv_line(ser)
+            #print('read data is: ' + str(data))
+            if data == b'\rok2\r\n':
+                break
             else:
                 print('fail to handshake')
-
-            
-        ser.write(123)
-        data = recv(ser)
-        print(data.decode("utf-8"))
-        break
+        else:
+            print('fail to handshake')
+    print("the picture labels is")
+    print("----begin download----")
+    for i in range(784):
+        num = i % 256
+        ser.write(bytes([num]))
+        data = recv_line(ser)
+    print("-----------------finish download-----------------")
+    while True:
+        data = recv_line(ser)
+        print(bytes.decode(data))
+        if data ==b'\rfinish\r\n':
+            break
 
